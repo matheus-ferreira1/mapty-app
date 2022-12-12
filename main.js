b46a0df86879e875420bf05678a7fb349345a259
Matheus Ferreira <matheus.fer33@gmail.com>
2022-12-11 21:03:29 -0300

    leaflet configuration for new pins

--- main.js
+++ main.js
@@ -42,10 +42,23 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+      map.on("click", (mapEvent) => {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("New pin added")
+          .openPopup();
+      });
     },
     () => alert("Could not get your position")
   );
