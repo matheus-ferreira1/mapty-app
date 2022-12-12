1519094fb57bc29ee7d0c409827c9b2f0812bcd6
Matheus Ferreira <matheus.fer33@gmail.com>
2022-12-11 21:26:48 -0300

    adding a pin reveals the form and added functionality to the select form

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import { htmlInjection } from "./htmlInjection.js";
 
-// insertion of the whole html content
+// injection of the whole html content
 htmlInjection();
 
 const months = [
@@ -27,39 +27,62 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+let map, mapEvent;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
+    //1st function is called when getcurrentposition gets coords
     (position) => {
       const { latitude, longitude } = position.coords;
-      console.log(`https://www.google.com.br/maps/@${latitude},${longitude}`);
 
       const coords = [latitude, longitude];
 
-      const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
 
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on("click", (mapEvent) => {
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: "running-popup",
-            })
-          )
-          .setPopupContent("New pin added")
-          .openPopup();
+      map.on("click", (mapE) => {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
       });
     },
+    //2nd function is called if getcurrentposition returns an error
     () => alert("Could not get your position")
   );
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //clearing input fields
+  inputCadence.value =
+    inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+      "";
+
+  //displaying the new marker
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("New pin added")
+    .openPopup();
+});
+
+inputType.addEventListener("change", () => {
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
